0567849fca9c22be6f11b4da7d6d7f196bf54d49
iMAC <iMAC@mbp-de-macbook.home>
2019-02-24 21:29:17 +0100

    Pájaro salta

--- starter_code/script.js
+++ starter_code/script.js
@@ -17,12 +17,13 @@ function Game(){
   this.child=document.getElementById("game-board");
   document.body.insertBefore(this.canvas, this.child.nextSibling);
   this.fps = 60;
-
   this.reset();
 }
 
 Game.prototype.reset = function() {
   this.background = new Background(this);
+  this.player = new Player(this);
+
 };
 
 Game.prototype.clear = function() {
@@ -31,6 +32,8 @@ Game.prototype.clear = function() {
 
 Game.prototype.draw = function() {
   this.background.draw();
+  this.player.draw();
+
 };
 
 
@@ -54,16 +57,11 @@ Game.prototype.clear = function() {
   this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 }; 
 
-Game.prototype.draw = function() {
-  this.background.draw();
-  //this.player.draw();
-  //this.obstacles.forEach(function(obstacle) { obstacle.draw(); });
-  //this.drawScore();  
-};
+
 
 Game.prototype.moveAll = function() {
   this.background.move();
-  //this.player.move();
+  this.player.move();
   //this.obstacles.forEach(function(obstacle) { obstacle.move(); });
 };
 
@@ -92,7 +90,65 @@ Background.prototype.move = function() {
   if (this.x < -this.game.canvas.width) this.x = 0;
 };
 
+function Player(game) {
+  this.game = game;
+ 
+  this.x = 100;
+
+  // guardar posición original (suelo)
+  this.y = 350;
+ 
+  this.y0 = 350;
+  this.y = this.y0;
+
+  this.img = new Image();
+  this.img.src = 'images/flappy.png';
+
+  // medidas de la imagen a representar en el canvas
+  this.w = 50;
+  this.h = 75;
+
+  this.vy = 1;
+
+
+  this.setListeners();
+}
 
 
 
-  
+
+Player.prototype.draw = function() {
+  // Documentación drawImage:
+  // https://developer.mozilla.org/es/docs/Web/API/CanvasRenderingContext2D/drawImage
+ 
+
+  this.game.ctx.drawImage(this.img, this.x, this.y, 100, 150);
+
+};
+
+var SPACE = 38;
+
+
+Player.prototype.setListeners = function() {
+  document.onkeydown = function(event) {
+    //alert('te oigo');
+    if (event.keyCode === SPACE && this.y == this.y0) {
+      this.y -= 5;
+      this.vy -= 50;
+    } 
+  }.bind(this);
+};
+
+Player.prototype.move = function() {
+  // Aumenta la velocidad en el eje y.
+  var gravity = 10;
+
+  // solo salta cuando el personaje está en el suelo
+ if (this.y >= this.y0) {
+    this.vy = 1;
+    this.y = this.y0;
+  } else{
+    this.vy += gravity;
+    this.y += this.vy;
+  }
+};
